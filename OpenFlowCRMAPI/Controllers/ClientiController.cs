6e794de673c2a1ac2a748a8efdf8ff014f8e32dd
Lucaf129 <lucafrancescon129@gmail.com>
2023-10-05 14:33:22 +0200

    Added Controller

---
+++ OpenFlowCRMAPI/Controllers/ClientiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OpenFlowCRMModels.Models;
+
+namespace OpenFlowCRMAPI.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ClientiController : Controller
+    {
+        private readonly SQL_TESTContext _context;
+
+        public ClientiController(SQL_TESTContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        [HttpGet(nameof(LottiNonAssegnati))]
+        public ActionResult<List<Lotti>> LottiNonAssegnati()
+        {
+            var lotti = _context.Lotti.Where(l => l.MacchinaAssegnata==null || l.MacchinaAssegnata < 0).ToList();
+
+            return Ok(lotti);
+        }
+    }
+}
