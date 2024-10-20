dc8a4b0e59a7f316b2a86ed21692ff528ac5a7f5
lucaf8664 <lucafrancescon8664@outlook.it>
2024-10-20 10:12:26 +0200

    Impostato il codice per l'autenticazione

--- OpenFlowCRMApp/Pages/Index.cshtml.cs
+++ OpenFlowCRMApp/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
+using Azure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json.Linq;
 using OpenFlowCRMModels.DTOs;
 
 namespace OpenFlowCRMApp.Pages
@@ -7,18 +9,15 @@ namespace OpenFlowCRMApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
 
         [BindProperty]
         public LoginDTO Utente { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
         {
             _logger = logger;
-        }
-
-        public void OnPost() 
-        { 
-        
+            _httpClientFactory=httpClientFactory;
         }
 
 
@@ -26,5 +25,27 @@ namespace OpenFlowCRMApp.Pages
         {
 
         }
+
+        public async Task<IActionResult> OnPostAsync() 
+        {
+
+            var httpClient = _httpClientFactory.CreateClient("CRM_API");
+            var httpResponseMessage = await httpClient.PostAsJsonAsync("api/Utenti/authenticate", Utente);
+
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                // show home page
+                var tokenString = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                HttpContext.Session.SetString("SessionToken", tokenString);
+                
+                return RedirectToPage("/Home");
+            }
+
+            return Page();
+
+        }
+
+
     }
 }
--- OpenFlowCRMApp/Program.cs
+++ OpenFlowCRMApp/Program.cs
@@ -3,6 +3,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddHttpClient("CRM_API", httpClient =>
+{
+    httpClient.BaseAddress = new Uri(builder.Configuration["API:URL"]);
+
+});
+
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -19,6 +34,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapRazorPages();
 
