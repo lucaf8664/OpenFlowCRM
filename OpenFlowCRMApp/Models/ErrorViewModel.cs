7245e5f623f598a1836c801e079da81e03e24d27
lucaf8664 <lucafrancescon8664@outlook.it>
2024-10-22 10:20:22 +0200

    ripartiamo. cancelliamo il progetto

--- OpenFlowCRMApp/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OpenFlowCRMApp.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
